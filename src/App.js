037fbbd862924cdb952b648b5677c469c15cac3d
dbwhdtjr0457 <dbwhdtjr0457@gmail.com>
2023-11-11 18:51:34 +0900

    setting

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    document.title = `You clicked ${count} times`;
-  });
-
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
